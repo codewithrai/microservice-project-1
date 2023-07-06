174f3e5aaa46e0af5d631db0b07c89b25e4c638e
Waqar Ahmad <wahmad@epteck.com>
2023-07-06 17:03:38 +0500

    eureka server add

--- inventory-service/src/main/java/com/progammingtechie/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/progammingtechie/InventoryServiceApplication.java
@@ -5,32 +5,34 @@ import com.progammingtechie.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
-		return args -> {
-			if (inventoryRepository.findBySkuCode("iphone_13").isEmpty()) {
-				Inventory inventory = new Inventory();
-				inventory.setSkuCode("iphone_13");
-				inventory.setQuantity(100);
-				inventoryRepository.save(inventory);
-			}
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            if (inventoryRepository.findBySkuCode("iphone_13").isEmpty()) {
+                Inventory inventory = new Inventory();
+                inventory.setSkuCode("iphone_13");
+                inventory.setQuantity(100);
+                inventoryRepository.save(inventory);
+            }
 
-			if (inventoryRepository.findBySkuCode("iphone_13_red").isEmpty()) {
-				Inventory inventory2 = new Inventory();
-				inventory2.setSkuCode("iphone_13_red");
-				inventory2.setQuantity(0);
-				inventoryRepository.save(inventory2);
-			}
-		};
-	}
+            if (inventoryRepository.findBySkuCode("iphone_13_red").isEmpty()) {
+                Inventory inventory2 = new Inventory();
+                inventory2.setSkuCode("iphone_13_red");
+                inventory2.setQuantity(0);
+                inventoryRepository.save(inventory2);
+            }
+        };
+    }
 
 }
